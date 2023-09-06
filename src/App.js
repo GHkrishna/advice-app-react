efe6f7778df65840e0ea17918f56c4ead43bf030
GHkrishna <kish77na@gmail.com>
2023-09-06 23:07:36 +0530

    Compeleted functional based get advice app

--- src/App.js
+++ src/App.js
@@ -1,67 +1,33 @@
-import React from 'react';
-import axios from 'axios'; //'npm install --save axios' use this before using axios
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import './App.css';
+const App = () => {
+  const [advice, setAdvice] = useState("");
 
-import Advice from './components/Advice';
-import PreviousAdvice from './components/PreviousAdvice';
+  function getData(){
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const adviceText = response.data.slip.advice;
+        setAdvice(adviceText);
+      })
+      .catch((error) => {
+        console.error("Error fetching advice:", error);
+      });
+    }
 
-// class App extends React.Component{
-//     state = {
-//         advice:''
-//     };
+  useEffect(() => {
+    getData();
+  }, []);
 
-//     componentDidMount(){
-//         console.log('COMPONENT DID MOUNT');
-//         setRecentQuote = {...recentQuote, }
-//         this.fetchQuote();
-//     };
-
-//     const[recentQuote, setRecentQuote] = useState([]);
-
-//     displayRecentQuote
-
-//     fetchQuote = ()=>{
-//         axios.get('https://api.adviceslip.com/advice')
-//         .then((response) => {
-//             const {advice} = response.data.slip;
-
-//             this.setState({advice: advice}); 
-
-//             //Or this.setState((advice)) If both state and local variables have same name
-
-//             console.log(advice);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     }
-
-//     render(){
-//         const {advice} = this.state; 
-//         return(
-//             <div className='app'>
-//                 <div className="card"> 
-//                     <h1 className = "heading"> {advice}</h1>
-//                     <button className='button' onClick={this.fetchQuote}>
-//                         <span>
-//                             GIVE ME ADVICE
-//                         </span>
-//                     </button>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-const App = ()=>{
-    return(
-        <div>
-            <Advice />
-            <PreviousAdvice />
-        </div>
-    );
+  return (
+    <div className="app">
+    <div className="card">
+      <h1>{advice}</h1>
+      <button className="button" onClick={getData}>Get Advice</button>
+    </div>
+    </div>
+  );
 }
 
 export default App;
